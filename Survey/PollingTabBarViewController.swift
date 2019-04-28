62fb4afbd8aa4227fb3e1d6fadb822eb37d67f0d
putergeek <putergeek7@outlook.com>
2019-04-27 21:13:04 -0700

    Setup Storyboard and Vote Class

---
+++ Survey/PollingTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PollingTabBarViewController.swift
+//  Survey
+//
+//  Created by Pat on 4/27/19.
+//  Copyright © 2019 putergeek. All rights reserved.
+//
+
+import UIKit
+
+class PollingTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Survey/Vote.swift
@@ -0,0 +1,22 @@
+//
+//  Vote.swift
+//  Survey
+//
+//  Created by Pat on 4/27/19.
+//  Copyright © 2019 putergeek. All rights reserved.
+//
+
+import Foundation
+
+
+class Vote {
+    
+    var name: String
+    var response: String
+    
+    init(name: String, response: String) {
+        
+        self.name = name
+        self.response = response
+    }
+}
